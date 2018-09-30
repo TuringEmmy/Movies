a64bdc67f2ea1c930c99a022362a64f1d27b3cfc
TuringEmmy <yonglonggeng@163.com>
2018-09-30 17:23:53 +0800

    add login and logout page

--- app/home/views.py
+++ app/home/views.py
@@ -1,8 +1,18 @@
 # coding=utf-8
 from . import home
-from flask import render_template
+from flask import render_template,redirect,url_for
 
+# 主页
 @home.route("/")
 def index():
     # return "<h1 style='color:green'>This is home</h1>"
     return render_template("home/index.html")
+
+# 登陆
+@home.route("/login/")
+def login():
+    return render_template("home/login.html")
+# 退出
+@home.route("/logout/")
+def logout():
+    return redirect(url_for("home.login"))
