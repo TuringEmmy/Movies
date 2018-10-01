aef4cd704d0434108a1846e9756ca4fc05fc1bdf
TuringEmmy <yonglonggeng@163.com>
2018-10-01 11:49:07 +0800

    accomplish register.html page

--- app/home/views.py
+++ app/home/views.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from . import home
-from flask import render_template,redirect,url_for
+from flask import render_template, redirect, url_for
+
 
 # 主页
 @home.route("/")
@@ -8,11 +9,20 @@ def index():
     # return "<h1 style='color:green'>This is home</h1>"
     return render_template("home/index.html")
 
+
 # 登陆
 @home.route("/login/")
 def login():
     return render_template("home/login.html")
+
+
 # 退出
 @home.route("/logout/")
 def logout():
     return redirect(url_for("home.login"))
+
+
+# 注册
+@home.route('/register/')
+def register():
+    return render_template("home/register.html")
