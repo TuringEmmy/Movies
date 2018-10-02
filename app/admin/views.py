99a5e661d91c6cfa6380f4f251dedea22c8fddc2
TuringEmmy <yonglonggeng@163.com>
2018-10-02 11:16:45 +0800

    BG bullit index.html

--- app/admin/views.py
+++ app/admin/views.py
@@ -5,7 +5,7 @@ from flask import render_template,redirect,url_for
 
 @admin.route("/")
 def index():
-    return "<h1 style='color:red'>This is admin</h1>"
+    return render_template("admin/index.html")
 
 # 管理员登录页面
 @admin.route('/login/')
