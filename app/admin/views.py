6e1fe39d4fc607f96c63062a671f696a9748a634
TuringEmmy <yonglonggeng@163.com>
2018-10-02 10:29:49 +0800

    admin login and logout page

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,7 +1,18 @@
 # coding=utf-8
 from . import admin
+from flask import render_template,redirect,url_for
 
 
 @admin.route("/")
 def index():
     return "<h1 style='color:red'>This is admin</h1>"
+
+# 管理员登录页面
+@admin.route('/login/')
+def login():
+    return render_template("admin/login.html")
+
+# 管理员退出页面
+@admin.route('/logout/')
+def logout():
+    return redirect(url_for('admin.login'))
