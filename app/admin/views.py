a5df389e83c7165bc6652bb53ff63425ff8109e7
TuringEmmy <yonglonggeng@163.com>
2018-10-02 16:30:38 +0800

    BG bullit preview and user panel

--- app/admin/views.py
+++ app/admin/views.py
@@ -23,12 +23,44 @@ def pwd():
     return render_template("admin/pwd.html")
 
 
-# 修改密码页面
+# 标签添加页面
 @admin.route('/tag/add')
 def tag_add():
     return render_template("admin/tag_add.html")
 
-# 修改密码页面
+# 标签列表页面
 @admin.route('/tag/list')
 def tag_list():
     return render_template("admin/tag_list.html")
+
+# 添加电影
+@admin.route('/movie/add')
+def movie_add():
+    return render_template("admin/movie_add.html")
+
+# 电影列表
+@admin.route('/movie/list')
+def movie_list():
+    return render_template("admin/movie_list.html")
+
+# 预告添加
+@admin.route('/preview/add')
+def preview_add():
+    return render_template("admin/preview_add.html")
+
+# 预告列表
+@admin.route('/preview/list')
+def preview_list():
+    return render_template("admin/preview_list.html")
+
+
+# 会员列表
+@admin.route('/user/list')
+def user_list():
+    return render_template("admin/user_list.html")
+
+# 查看会员
+@admin.route('/user/view')
+def user_view():
+    return render_template("admin/user_view.html")
+
