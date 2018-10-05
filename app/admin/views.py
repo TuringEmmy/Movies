442fc405f1e96ecb7ea97c8f24118bbf706848ff
TuringEmmy <yonglonggeng@163.com>
2018-10-05 15:47:42 +0800

    adminloginlog_list is over

--- app/admin/views.py
+++ app/admin/views.py
@@ -73,6 +73,15 @@ def login():
         session["admin"] = data["account"]
         # 还要保存icu你session,对应下面的登出函数的admin_id的销毁
         session["admin_id"] = admin.id
+        # -----------------------管理员登陆日志-----------------------
+        adminlog = Adminlog(
+            admin_id=admin.id,
+            ip=request.remote_addr
+        )
+        db.session.add(adminlog)
+        db.session.commit()
+        # -----------------------管理员登陆日志 - ----------------------
+
         return redirect(request.args.get("next") or url_for("admin.index"))
     return render_template("admin/login.html", form=form)
 
@@ -523,10 +532,19 @@ def oplog_list(page=None):
 
 
 # 管理员登陆日至
-@admin.route('/adminloginlog/list/')
+@admin.route('/adminloginlog/list/<int:page>', methods=["GET"])
 @admin_login_req
-def adminloginlog_list():
-    return render_template("admin/adminloginlog_list.html")
+def adminloginlog_list(page=None):
+    if page is None:
+        page = 1
+    page_data = Adminlog.query.join(
+        Admin
+    ).filter(
+        Admin.id == Adminlog.admin_id,
+    ).order_by(
+        Adminlog.addtime.desc()
+    ).paginate(page=page, per_page=10)
+    return render_template("admin/adminloginlog_list.html", page_data=page_data)
 
 
 # 会员员登陆日至
